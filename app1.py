ee04a8d072a17482f3cc9d97e23a490f493b1e6a
Patrick Hess <phess.ph@gmail.com>
2021-09-01 15:18:22 -0700

    Added fun mesasge and link

--- app1.py
+++ app1.py
@@ -15,6 +15,7 @@ Created on Mon Oct 14 20:46:43 2019
 import os
 import json
 from flask import Flask
+from flask import render_template, make_response
 from flask_restful import reqparse, abort, Api, Resource, request
 import markovify as mk
 
@@ -47,6 +48,12 @@ reconstituted_model_michael = mk.Text.from_json(michael_model_json)
 reconstituted_model_dwight = mk.Text.from_json(dwight_model_json)
 reconstituted_model_jim = mk.Text.from_json(jim_model_json)
 reconstituted_model_pam = mk.Text.from_json(pam_model_json)
+
+
+class landing(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'),200,headers)
 
 
 class MichaelModel(Resource):
@@ -101,11 +108,12 @@ class HelloTori(Resource):
         return {'Hello': 'I love you! <3'}
 
 
+api.add_resource(landing, "/")
 api.add_resource(MichaelModel, '/michael_model')
 api.add_resource(JimModel, '/jim_model')
 api.add_resource(PamModel, '/pam_model')
 api.add_resource(DwightModel, '/dwight_model')
-api.add_resource
+api.add_resource(HelloTori, "/luvu")
 
 if __name__ == '__main__':
     #app.run(debug=True)
